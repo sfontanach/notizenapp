82936c05f9ed1f74e4b4b5e21c57fb683eae0063
Stefano Fontana <fontana.stefano0@gmail.com>
2017-10-05 09:55:39 +0200

    Use local image for star.png.

--- notizenapp/Startup.cs
+++ notizenapp/Startup.cs
@@ -30,6 +30,7 @@ namespace notizenapp
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+
         {
             if (env.IsDevelopment())
             {
